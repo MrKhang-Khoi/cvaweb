2681e3f11d73beb0536f856c31948b4894677a13
MrKhang-Khoi <havanty84@gmail.com>
2026-09-17 12:55:53 +0700

    Feat: Them quan ly danh muc chinh cho Admin & nang cap ma hoa mat khau Salted SHA-256 chuan W3C WebCrypto

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /* ==========================================================================
    SERVICE WORKER: CỔNG WEBSITE GIÁO VIÊN (PWA OFFLINE-FIRST & AUTO-UPDATE)
-   Phiên bản: 1.1.0
+   Phiên bản: 1.2.0
    ========================================================================== */
 
-const CACHE_NAME = 'teacher-hub-v1.1.0';
+const CACHE_NAME = 'teacher-hub-v1.2.0';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
